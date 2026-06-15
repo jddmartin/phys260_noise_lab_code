b68c9d030133d6efedc1f04dc3f0d42d3a7101cb
James D. D. Martin <jddmartin@uwaterloo.ca>
2026-06-15 15:31:24 -0400

    Many updates

--- keysight_dmm_measure.py
+++ keysight_dmm_measure.py
@@ -2,16 +2,19 @@
 
 # written by J. Martin, U Waterloo
 
-import pyvisa
-import time
-import numpy as np
-import subprocess
-import os
+import time, subprocess, os, argparse, __main__
 
-def main():
+import pyvisa
+import numpy as np
+np.set_printoptions(legacy='1.25')
+
+def main(args):
+    n_samples = args["n_samples"]
+    debug = args["debug"]
+
     rm = pyvisa.ResourceManager()
     rs = rm.list_resources()
-    print(f"{rs=}")
+    if debug: print(f"{rs=}")
     for r in rs:
         if "::0x2A8D::0x1301::" in r:  # VISA name for Keysight 34461A
             inst = rm.open_resource(r)
@@ -19,7 +22,7 @@ def main():
     else:
         raise RuntimeError("Could not detect Keysight 34461A.  Is it connected?")
     try:
-        print(inst.query("*IDN?"))    
+        if debug: print(f'{inst.query("*IDN?")=}')    
         inst.timeout = 1000000
 
         # store the state of the instrument so that we go back to it after
@@ -37,6 +40,9 @@ def main():
         inst.write("SAMPLE:COUNT " + str(n_samples))
 
         # make measurement:
+        print("Measuring ... will take approximately: ",
+              "%.1f" % (n_samples * 2.6),
+              "(s) =(%.1f" % (n_samples * 2.6 / 60), "(min))")
         start = time.time()
         while True:
             results = inst.query("READ?")
@@ -55,16 +61,40 @@ def main():
         est_std_avg_vrms2 = np.sqrt(np.std(vals**2)**2 / (len(vals) - 1))
 
         # print results to screen:
+        print("\nSuccess!\n")
         print(f"{time_elapsed_s=}")
 
-    except:
-        print("except")
+        print(f"{np.sqrt(avg_vrms2)=} V")
+        print()
+        print(f"{avg_vrms2=} V^2")
+        print(f"{est_std_avg_vrms2=} V^2")        
+
+    except Exception as e:
+        print(e)        
         inst.control_ren(6)
 
     if os.name == "posix":  # "posix" is linux, "nt" is windows
         subprocess.call(["espeak", "I am finished, my friend."])
 
+def parse_args():
+    example_of_use = "Example usage:\n " +  __main__.__file__ + " --debug 1"
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     epilog=example_of_use,
+                                     formatter_class=
+                                     argparse.RawTextHelpFormatter)
+    parser.add_argument("--debug",
+                        help = "print additional debug info ",
+                        default=False, action="store_true")
+
+    parser.add_argument("n_samples",
+                        help = "number of samples ")
+
+    # we return a dictionary, as this is easy to create in case we
+    # want to drive program without command line arguments
+    # (see: https://stackoverflow.com/q/16878315 ):
+    return vars(parser.parse_args())
+        
 if __name__ == "__main__":
     # each sample takes 2.6 s
-    
-    main()
+    args = parse_args()
+    main(args)
