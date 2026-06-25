ad07073daeab16ec62aff04704fb8768e9184651
James D. D. Martin <jddmartin@uwaterloo.ca>
2026-06-25 13:48:00 -0400

    Make Pt1000 program a bit more user friendly

--- temperature_from_pt1000.py
+++ temperature_from_pt1000.py
@@ -4,7 +4,7 @@
 # https://en.wikipedia.org/wiki/Resistance_thermometer#Standard_resistance_thermometer_data
 
 
-import sys
+import sys, argparse, __main__
 import numpy as np
 np.set_printoptions(legacy='1.25')
 
@@ -20,7 +20,31 @@ def temp_for_rtd(rtd):
     t = (-a + np.sqrt(a**2 - 4 * b * (1 - x))) / (2.0 * b)
     return t
 
-if __name__ == "__main__":
-    resistance = float(sys.argv[1])
+
+def main(args):
+    resistance = float(args["resistance"])
     temperature = temp_for_rtd(resistance)
-    print(f"for {resistance=} (Ohms) {temperature=} (deg C)")
+    print(f"for {resistance=} (Ohms)\n{temperature=} (deg C)")
+
+
+def parse_args():
+#    example_of_use = "Example usage:\n " +  __main__.__file__ + " --debug 1"
+    example_of_use = ""
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     epilog=example_of_use,
+                                     formatter_class=
+                                     argparse.RawTextHelpFormatter)
+    parser.add_argument("--debug",
+                        help = "print additional debug info ",
+                        default=False, action="store_true")
+
+    parser.add_argument("resistance", type=float,
+                        help = "Pt1000 resistance in ohms ")
+
+    return vars(parser.parse_args())  # return dictionary
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
+    
